5ffb9d23addec2728f83c87a120d70a76742b3e4
Adi Kurniawan <kurniawan_01@ymail.com>
2024-04-18 14:17:14 +0700

    see details of errors

--- public/index.php
+++ public/index.php
@@ -46,6 +46,10 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
